65121c601c08294b4a1817242acce4ea57b974cf
HuseynSadatkhanovHolberton <12671@holbertnstudents.com>
2026-01-11 17:04:27 +0000

    main.h

--- main.h
+++ main.h
@@ -1,15 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-
-/* _printf prototype */
 int _printf(const char *format, ...);
-
-/* Helper functions */
-int print_char(char c);
-int print_string(char *s);
 
 #endif /* MAIN_H */
 
